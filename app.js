524bdf875d36300e34552c3c9cc3939b27339239
Tim <tim.weinert@gmail.com>
2020-01-29 11:00:19 +1100

    2 random numbers generated

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ function rollDice(min, max) {
     console.log (Math.floor(Math.random() * (max - min)) + min); 
 };
 
+function rollDice2(min, max) {
+    min = Math.ceil(1); //Math.ciel() rounds up to whole integer
+    max = Math.floor(7);//Math.floor() rounds down to whole integer
+
+    console.log (Math.floor(Math.random() * (max - min)) + min); 
+};
+
+
 //setInterval(rollDice, 1000);
 
 //When .btn is clicked, init the function
 rollDiceButton.addEventListener('click', rollDice)
+rollDiceButton.addEventListener('click', rollDice2)
