908c82c7e2c02c35002c2ff8ea48c539177ebbe1
Tim <tim.weinert@gmail.com>
2020-01-29 12:29:38 +1100

    Toggle card when clicked

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-
+//VARIABLES
 //Variable for roll dice button location
 const rollDiceButton = document.querySelector('.btn');
+const flipCard = document.querySelector('.flip-card-inner');
+const flipCardA = document.querySelector('.flip-card-front');
+const flipCardB = document.querySelector('.flip-card-back');
 
+//FUNCTIONS
 //Roll dice to a random number
 function rollDice(min, max) {
     min = Math.ceil(1); //Math.ciel() rounds up to whole integer
@@ -17,9 +21,20 @@ function rollDice2(min, max) {
     console.log (Math.floor(Math.random() * (max - min)) + min); 
 };
 
+//Flip the card when it is clicked using toggle
+function flipCards (){
+    this.classList.toggle('flip-card-inner-transform');
+}
+
+
 
 //setInterval(rollDice, 1000);
 
+//EVENT LISTENERS
 //When .btn is clicked, init the function
 rollDiceButton.addEventListener('click', rollDice)
 rollDiceButton.addEventListener('click', rollDice2)
+
+flipCard.addEventListener('click', flipCards)
+
+
